61a93d329d218152bba109523741e4d8a194e6b2
unknown <lne128gene980@163.com>
2020-04-05 01:26:41 +0800

    面板过滤器

--- src/filters/index.js
+++ src/filters/index.js
@@ -1,8 +1,8 @@
-let ff = (val, key, num = 2, symbol = '%') => {
+let ff = (val, key, num = 2) => {
   let popsList1 = ['暴击', '攻击加成', '暴击伤害', '生命加成', '防御加成', '效果命中', '效果抵抗'];
   let popsList2 = ['攻击', '防御', '速度', '生命'];
   if (popsList1.includes(key)) {
-    return `${(val * 100).toFixed(num)}${symbol}`;
+    return `${(val * 100).toFixed(num)}%`;
   } else if (popsList2.includes(key)) {
     return `${val.toFixed(num)}`;
   } else {
@@ -10,4 +10,14 @@ let ff = (val, key, num = 2, symbol = '%') => {
   }
 };
 
-export { ff };
+let paneFilter = (val, key) => {
+  if (['暴击', '暴击伤害', '效果命中', '效果抵抗'].includes(key)) {
+    return `${val * 100}%`;
+  } else if (val == 0 || key == '速度') {
+    return val;
+  } else {
+    return `${val.toFixed(2)}`;
+  }
+};
+
+export { ff, paneFilter };
